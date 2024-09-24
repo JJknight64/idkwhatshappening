b53163437054b333de77ecb5dd8bac4f452d3ac5
JJknight64 <jwd8744@rit.edu>
2024-09-24 08:58:13 -0400

    added basic integer / variable experimentation

--- idkwhatshappening/Program.cs
+++ idkwhatshappening/Program.cs
@@ -5,6 +5,14 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            int myFavoriteNumber = 3;
+            int theBestNumber = 17;
+
+            int sum;
+
+            sum = myFavoriteNumber + theBestNumber;
+            Console.WriteLine(sum);
         }
     }
 }
